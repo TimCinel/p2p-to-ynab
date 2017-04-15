8ab5617950756d1bcac62759ca08aa0b7f482253
Tim Cinel <email@timcinel.com>
2017-04-16 04:30:25 +1000

    Update truepillars.js to support official CSV output

--- truepillars.js
+++ truepillars.js
@@ -1,4 +1,4 @@
-var truepillarsFields = '"Ref","Date","Description","Account","Debit","Credit","Balance",'.split(",")
+var truepillarsFields = 'Ref,Date,Description,Debit,Credit,Balance'.split(",")
 
 function stripQuotes(string) {
   return string.replace(/"/g, "");
@@ -10,25 +10,20 @@ function transformTransaction(transaction, ynabConfig) {
     return null;
   }
 
+  function dateSwizzle(date) {
+    return transaction['Date'].split("-").reverse().join("/");
+  }
+
   function beginsWith(type) {
-    return transaction['"Description"'].search(type["match"]) == 0
+    return transaction['Description'].search(type["match"]) == 0
   }
 
   function getCSV(type) {
-    return type["getCSV"](transaction['"Description"'].substring(type["match"].length))
-  }
-
-  function dateJiggle() {
-      months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06",
-              "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
-
-    date_parts = stripQuotes(transaction['"Date"']).split(" ")
-    date_parts[1] = months[date_parts[1]]
-    return date_parts.join("/")
+    return type["getCSV"](transaction['Description'].substring(type["match"].length))
   }
 
   function cleanMoney(key) {
-    return stripQuotes(transaction['"' + key + '"'].replace("$", "").replace(",", ""))
+    return stripQuotes(transaction[key].replace("$", "").replace(",", ""))
   }
 
   // 07/25/10,Sample Payee,,Sample Memo for an outflow,100.00,
@@ -42,19 +37,19 @@ function transformTransaction(transaction, ynabConfig) {
     {"match": "Interest earned for investment ",
      "type": "interest",
      "getCSV": function(item) {
-        return [dateJiggle(), ynabConfig["interestPayee"], ynabConfig["interestCategory"], "Interest payment from " + item, "", cleanMoney("Credit")].join(",");
+        return [dateSwizzle(), ynabConfig["interestPayee"], ynabConfig["interestCategory"], "Interest payment from " + item, "", cleanMoney("Credit")].join(",");
      }
     },
     {"match": "Capital returned for investment ",
      "type": "repayment",
      "getCSV": function(item) {
-        return [dateJiggle(), ynabConfig["loanAccount"], ynabConfig["capitalCategory"], "Capital repayment from " + item, "", cleanMoney("Credit")].join(",");
+        return [dateSwizzle(), ynabConfig["loanAccount"], ynabConfig["capitalCategory"], "Capital repayment from " + item, "", cleanMoney("Credit")].join(",");
      }
     },
     {"match": "Cash withdrawal",
      "type": "withdrawal",
      "getCSV": function(item) {
-        return [dateJiggle(), ynabConfig["withdrawAccount"], "", "Withdrawal", cleanMoney("Debit"), ""].join(",");
+        return [dateSwizzle(), ynabConfig["withdrawAccount"], "", "Withdrawal", cleanMoney("Debit"), ""].join(",");
      }
     }
   ]
